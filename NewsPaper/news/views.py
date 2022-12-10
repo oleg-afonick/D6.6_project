7c7157661391a1f5044a0812f5ee9246d6214947
oleg-afonick <oleg.afonick@gmail.com>
2022-12-10 17:18:18 +0300

    D6.3

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,12 +1,19 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
 
 
 class NewsList(ListView):
     model = Post
-    ordering = 'post_title'
+    ordering = '-date_creation'
     template_name = 'news.html'
     context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = None
+        return context
 
 
 class NewsDetail(DetailView):
